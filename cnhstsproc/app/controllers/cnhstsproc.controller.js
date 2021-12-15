5a2c71abec85545174a68feb87fb10d08f4043d8
renatocamara <camarabox@gmail.com>
2021-12-15 10:59:40 -0300

    first commit

--- cnhstsproc/app/controllers/cnhstsproc.controller.js
+++ cnhstsproc/app/controllers/cnhstsproc.controller.js
@@ -1,20 +1,5 @@
 const CNH_STS_PROC = require('../models/cnhstsproc.model')
 
-// exports.read = async(req, res) => {
-//     const cnh_sts_proc = await CNH_STS_PROC.read();
-//     res.status(200).send(cnh_sts_proc)
-// }
-
-// exports.readByCpfandCnhNro = async(req, res) => {
-//     const cnh_sts_proc = await CNH_STS_PROC.readByCpfandCnhNro(req.params.cpf, req.params.cnh_nro)
-
-//     if(cnh_sts_proc.length){
-//         res.status(200).send(cnh_sts_proc)
-//     } else {
-//         res.status(500).send({message: 'Not found.'})
-//     }
-// }
-
 exports.read = (req, res) => {
     CNH_STS_PROC.read((err, data)=>{
         if(err){
