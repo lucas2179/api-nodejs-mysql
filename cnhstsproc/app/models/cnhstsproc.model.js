c8bf085c169a77116eba957419c5212eb9e96bc5
renatocamara <camarabox@gmail.com>
2021-12-15 13:31:44 -0300

    fist commit

--- cnhstsproc/app/models/cnhstsproc.model.js
+++ cnhstsproc/app/models/cnhstsproc.model.js
@@ -1,26 +1,5 @@
 const { pool } = require('../../config/database')
 
-// exports.read = async () => {
-//     const pool = await poolPromise;
-//     const rs = await pool
-//         .request()
-//         .query(`SELECT *
-//                 FROM CNH_REN`)
-
-//     return rs.recordset;
-// }
-
-// exports.readByCpfandCnhNro = async(cpf,cnh_nro) =>{
-//     const pool = await poolPromise;
-//     const rs = await pool
-//             .request()
-//             .query(`SELECT nome as Nome, ren_desc as Descricao, ren_situacao as Situacao, ren_data as Data, ren_protocolo as Protocolo
-//                     FROM CNH_REN 
-//                     WHERE CPF = '${cpf}' AND CNH_NRO = '${cnh_nro}' `);
-    
-//     return rs.recordset;
-// }
-
 exports.read = (resultado) => {
     pool.query(`SELECT * FROM CNH_REN`, (err, res) => {
         if (err){
@@ -33,10 +12,7 @@ exports.read = (resultado) => {
 })
 }
 
-
 exports.readByCpfandCnhNro = (cpf, cnh_nro, resultado) => {
-    console.log("cpf: ", cpf);
-    console.log("conta: ", cnh_nro);
     pool.query(`SELECT nome as Nome, ren_desc as Descricao, ren_situacao as Situacao, ren_data as Data, ren_protocolo as Protocolo FROM CNH_REN WHERE CPF = '${cpf}' AND CNH_NRO = '${cnh_nro}' `, (err, res) => {
         if (err){
             console.log("error: ", err);
