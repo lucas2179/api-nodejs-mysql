ed60bca26247f18c2407fb53085ae9d4323e19b1
renatocamara <camarabox@gmail.com>
2021-12-16 11:11:20 -0300

    first commit

--- cnhboleto/app/controllers/cnhboleto.controller.js
+++ cnhboleto/app/controllers/cnhboleto.controller.js
@@ -1,16 +1,5 @@
 const CNH_BOLETO = require('../models/cnhboleto.model')
 
-// exports.getBoleto = async(req, res) => {
-//     const cnh_boleto = await CNH_BOLETO.getBoleto(req.params.cpf)
-
-//     if(cnh_boleto.length){
-//         res.status(200).send(cnh_boleto)
-//     } else {
-//         res.status(500).send({message: 'Not found.'})
-//     }
-// }
-
-
 exports.getBoleto = (req, res) => {
     CNH_BOLETO.getBoleto(req.params.cpf, (err, data)=>{
         if(err){
