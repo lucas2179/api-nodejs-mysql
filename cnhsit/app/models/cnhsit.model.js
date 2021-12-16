83f323e5ab1b5e6c6fa85b16fa9137a032b6c30d
renatocamara <camarabox@gmail.com>
2021-12-16 11:10:13 -0300

    first commit

--- cnhsit/app/models/cnhsit.model.js
+++ cnhsit/app/models/cnhsit.model.js
@@ -1,28 +1,5 @@
 const { pool} = require('../../config/database')
 
-// exports.read = async () => {
-//     const pool = await poolPromise;
-//     const rs = await pool
-//         .request()
-//         .query(`SELECT *
-//                 FROM CNH_SIT`)
-
-//     return rs.recordset;
-// }
-
-// exports.readByCpfandCnhNro = async(cpf,cnh_nro) =>{
-//     const pool = await poolPromise;
-//     const rs = await pool
-//             .request()
-//             .query(`SELECT CNH_Status as Situacao
-//                     FROM CNH_SIT 
-//                     WHERE CPF = '${cpf}' AND CNH_NRO = '${cnh_nro}' `);
-    
-//     return rs.recordset;
-// }
-
-
-
 exports.read = (resultado) => {
     pool.query(`SELECT * FROM CNH_SIT`, (err, res) => {
         if (err){
